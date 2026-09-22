990fe580fd337c8a7ad73a381738479665444299
Berzey81 <amber.l.trottier@gmail.com>
2026-09-21 17:16:48 -0700

    Initial custom WordPress theme

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+// Theme setup
+function fernwood_trail_setup() {
+    add_theme_support( 'title-tag' );
+}
+
+add_action( 'after_setup_theme', 'fernwood_trail_setup' );
+
+// Load the theme stylesheet
+function fernwood_trail_enqueue_styles() {
+    wp_enqueue_style( 'fernwood-trail-style', get_stylesheet_uri() );
+}
+
+add_action( 'wp_enqueue_scripts', 'fernwood_trail_enqueue_styles' );
+
+//Register navigation menu
+function fernwood_trail_register_menus() {
+    register_nav_menus(
+        array(
+            'priamry' => 'Primary Menu',
+        )
+    );
+}
+
+add_action('after_setup_theme', 'fernwood_trail_register_menus');
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+    <h1>Welcome to Fernwood Trail Co.</h1>
+    <p>This is my first custom WordPress theme.</p>
+</main>
+
+<?php get_footer(); ?>
